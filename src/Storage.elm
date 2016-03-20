8014fbd4ac2eafef70c6b0f370c63eff27c501b1
Dan Palmer <dan@danpalmer.me>
2016-03-20 14:41:42 +0000

    Docs and formatting

--- src/Storage.elm
+++ src/Storage.elm
@@ -1,4 +1,17 @@
-module Storage where
+module Storage (..) where
+
+{-| This library provides access to the HTML LocalStorage API
+through Tasks.
+
+# Documentation
+@docs getItemAsJson
+@docs getItem
+@docs setItem
+@docs removeItem
+@docs clear
+@docs keys
+@docs length
+-}
 
 import Json.Encode exposing (Value)
 import Json.Decode exposing (Decoder, decodeValue)
@@ -7,33 +20,59 @@ import List
 import Native.Storage
 
 
+{-| Get the value of a key from local storage, assuming it is JSON
+-}
 getItemAsJson : String -> Task String Value
-getItemAsJson = Native.Storage.getItemAsJson
+getItemAsJson =
+  Native.Storage.getItemAsJson
 
--- Do better error detection
+
+{-| Get the value of a key from local storage
+-}
 getItem : String -> Decoder value -> Task String value
 getItem key decoder =
-  let decode value = case decodeValue decoder value of
-    Ok v    -> succeed v
-    Err err -> fail "Failed"
+  let
+    decode value =
+      case decodeValue decoder value of
+        Ok v ->
+          succeed v
+
+        Err err ->
+          fail "Failed"
   in
     getItemAsJson key `andThen` decode
 
+
+{-| Set the value of a key in local storage
+-}
 setItem : String -> Value -> Task String ()
-setItem = Native.Storage.setItem
+setItem =
+  Native.Storage.setItem
 
+
+{-| Remove a key from local storage
+-}
 removeItem : String -> Task String ()
-removeItem = Native.Storage.removeItem
+removeItem =
+  Native.Storage.removeItem
 
+
+{-| Clear all local storage
+-}
 clear : Task String ()
-clear = Native.Storage.clear
+clear =
+  Native.Storage.clear
 
+
+{-| Get a list of all keys in local storage
+-}
 keys : Task String (List String)
-keys = Native.Storage.keys
-
--- keysArray : Task error (Array String)
--- keysArray : Native.Storage.keysArray
+keys =
+  Native.Storage.keys
 
 
+{-| Get the number of keys in local storage
+-}
 length : Task String Int
-length = Native.Storage.length
+length =
+  Native.Storage.length
